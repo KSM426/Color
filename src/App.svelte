8e97274d7a011a5f91be205a3312d83454f60e17
KSM <goegle111@gmail.com>
2023-06-21 12:27:15 +0900

    make show(paper), input, header will make function, save, copy, input design

---
+++ src/App.svelte
@@ -0,0 +1,90 @@
+<script>
+	import D0 from "./design0.svelte"
+	let colors = [
+		'A0C49D', 'C4D7B2', 'E1ECC8', '000000'
+	];
+	let design = 0;
+</script>
+
+<main>
+	<div id="top">
+		<div id="menu">mm</div>
+		<div id="hd">PALETTE</div>
+	</div>
+
+	<div id="content">
+		<D0 {colors}/>
+
+		<div id="function">
+			<div class="inFunc" id="share"></div>
+			<div class="inFunc" id="space"></div>
+			<div class="inFunc" id="copy"></div>
+			<div class="inFunc" id="save"></div>
+		</div>
+
+		{#each colors as color, idx}
+			<div>{idx+1} : <input bind:value="{color}" type="text"/></div>
+		{/each}
+	</div>
+</main>
+
+<style>
+	#top{
+		position: relative;
+		width: 100vw;
+		height: 13vh;
+	}
+	#menu{
+		position: absolute;
+		top: 5vh;
+		left: 5vw;
+		transform: translate(-50%, -50%);
+		color: red;
+	}
+	#hd{
+		position: absolute;
+		top: 5vh;
+		left: 50vw;
+		transform: translate(-50%, -50%);
+		font-size: 30px;
+		margin-top: 10px;
+		text-align: center;
+	}
+	#content{
+		width: 100vw;
+		text-align: center;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	#function{
+		width: 340px;
+		height: 50px;
+		background-color: #F7FFE5;
+		margin-bottom: 15px;
+		border: solid 0.5px black;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+	}
+	.inFunc{
+		height: 40px;
+		background-color: gray;
+	}
+	#share{
+		margin-left: 5px;
+		width: 40px;
+	}
+	#space{
+		margin-left: 5px;
+		width: 195px;
+	}
+	#copy{
+		margin-left: 5px;
+		width: 40px;
+	}
+	#save{
+		margin-left: 5px;
+		width: 40px;
+	}
+</style>
---
+++ src/design0.svelte
@@ -0,0 +1,55 @@
+<script>
+    export let colors;
+</script>
+
+<main>
+    <div id="show">
+        <div id="paper" style="box-shadow: #fff;">
+            {#each colors as color, idx}
+                {#if idx==0}
+                    <div id="in"
+                    style="background-color: #{color};
+                    margin-top: 10px;
+                    border-top-left-radius: 20px;
+                    border-top-right-radius: 20px;">{idx+1}</div>
+                {:else if idx==3}
+                    <div id="in"
+                    style="background-color: #{color};
+                    border-bottom-left-radius: 20px;
+                    border-bottom-right-radius: 20px;">{idx+1}</div>
+                {:else}
+                    <div id="in" style="background-color: #{color};">{idx+1}</div>
+                {/if}
+            {/each}
+        </div>
+    </div>
+</main>
+
+<style>
+    main{
+        text-align: center;
+    }
+    #show{
+        width: 100vw;
+        height: 340px;
+        display: flex;
+        justify-content: center;
+        margin-bottom: 15px;
+    }
+    #paper{
+        width: 340px;
+        height: 340px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        box-shadow: 3px 3px 10px;
+        border-radius: 20px;
+    }
+    #in{
+        text-align: center;
+        margin: 0;
+        padding: 0;
+        width: 320px;
+        height: 80px;
+    }
+</style>
